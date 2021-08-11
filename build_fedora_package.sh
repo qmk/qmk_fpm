99b6d433069b24be810e6b14826d1cb39bb32720
Zach White <skullydazed@gmail.com>
2021-08-11 09:44:59 -0700

    initial version of fedora package

---
+++ build_fedora_package.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+. /etc/os-release
+
+rm -rf qmk
+
+# Build the virtualenv
+mkdir -p qmk/etc/yum.repos.d qmk/usr/bin qmk/usr/lib/udev/rules.d qmk/usr/share/python
+python3 -m venv qmk/usr/share/python/qmk
+qmk/usr/share/python/qmk/bin/pip install qmk
+
+# Extract the version
+QMK_VERSION=$(qmk/usr/share/python/qmk/bin/qmk --version)
+
+# Make the virtualenv work in the final location
+virtualenv-tools --update-path /usr/share/python/qmk qmk/usr/share/python/qmk
+ln -s ../share/python/qmk/bin/qmk qmk/usr/bin/qmk
+
+# Copy in some other files
+cp 50-qmk.rules qmk/usr/lib/udev/rules.d/
+sed "s,%VERSION_ID%,${VERSION_ID}," qmk.repo > qmk/etc/yum.repos.d/qmk.repo
+
+# Build the fedora package
+cd qmk
+fpm \
+	--input-type dir \
+	--name qmk \
+	--version "${QMK_VERSION}" \
+	--description "Quantum Mechanical Keyboard (QMK) Firmware.
+
+QMK is an open source keyboard firmware that can build a firmware and custom
+layout for hundreds of keyboards, or your own custom design." \
+	--url "https://qmk.fm/" \
+	--output-type rpm \
+	--license expat \
+	--vendor QMK \
+	--category devel \
+	--architecture all \
+	--maintainer 'QMK Firmware (Official QMK GPG Signing Key) <hello@qmk.fm>' \
+	--rpm-sign \
+	--depends clang \
+	--depends diffutils \
+	--depends git \
+	--depends gcc \
+	--depends glibc-headers \
+	--depends kernel-devel \
+	--depends kernel-headers \
+	--depends make \
+	--depends unzip \
+	--depends wget \
+	--depends zip \
+	--depends python3 \
+	--depends avr-binutils \
+	--depends avr-gcc \
+	--depends avr-libc \
+	--depends arm-none-eabi-binutils-cs \
+	--depends arm-none-eabi-gcc-cs \
+	--depends arm-none-eabi-newlib \
+	--depends avrdude \
+	--depends dfu-programmer \
+	--depends dfu-util \
+	--depends hidapi \
+	.
---
+++ build_fedora_repo.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf rpm_repo
+
+if ! [ -e qmk ]; then
+	echo
+	echo '*** Building the qmk .rpm package:'
+	echo
+	./build_fedora_package.sh
+fi
+
+# Import the GPG key
+if [ -n "$QMK_GPG_PRIVATE_KEY" ]; then
+	echo '*** Importing GPG key...'
+	echo "$QMK_GPG_PRIVATE_KEY" | gpg --batch --import
+else
+	echo -e '\n*** Warning: Could not import GPG key!\n'
+fi
+
+set -x
+
+. /etc/os-release
+
+ARCHITECTURES="i386 amd64 arm arm64"
+
+# Create the repo structure
+mkdir -p rpm_repo
+cp qmk/*.rpm rpm_repo
+
+# Build the repo metadata
+createrepo -v -s md5 rpm_repo
+
+# Debugging information
+find rpm_repo -type f -ls
