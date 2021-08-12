3066fca1cddbfb2e0e06f50ddd38d6d1d4594648
Zach White <skullydazed@gmail.com>
2021-08-12 15:53:08 -0700

    Update build_fedora_package.sh
    Co-authored-by: Thomas Weißschuh <thomas@t-8ch.de>

--- build_fedora_package.sh
+++ build_fedora_package.sh
@@ -24,7 +24,6 @@ ln -s ../share/python/qmk/bin/qmk qmk/usr/bin/qmk
 # Install the qmk_udev package
 git clone https://github.com/qmk/qmk_udev.git
 cd qmk_udev
-make
 make DESTDIR=../qmk PREFIX=/usr install
 cd ..
 
